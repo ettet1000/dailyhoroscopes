2923113e603d1dda52f99613525f8d068d7d7317
ettet1001@gmail.com <76035965+ettet1000@users.noreply.github.com>
2022-08-30 14:22:24 +0900

    Update language_manager.dart

--- lib/core/init/lang/language_manager.dart
+++ lib/core/init/lang/language_manager.dart
@@ -11,6 +11,22 @@ class LanguageManager {
 
   final trLocale = const Locale('tr', 'TR');
   final enLocale = const Locale('en', 'US');
+  final deLocale = const Locale('de', 'DE');
+  final ruLocale = const Locale('ru', 'RU');
+  final viLocale = const Locale('vi', '');
+  final esLocale = const Locale('es', 'ES');
+  final arLocale = const Locale('ar', '');
+  final itLocale = const Locale('it', 'IT');
+  final jaLocale = const Locale('ja', 'JP');
+  final zhLocale = const Locale('zh', 'CN');
+  final ptLocale = const Locale('pt', 'PT');
+  final frLocale = const Locale('fr', 'FR');
+  final koLocale = const Locale('ko', 'KR');
+  
+  
+  
 
-  List<Locale> get supportedLocales => [enLocale, trLocale];
+
+  List<Locale> get supportedLocales => [enLocale, trLocale, deLocale, ruLocale
+  , viLocale, esLocale, arLocale, itLocale, jaLocale, zhLocale, ptLocale, frLocale, koLocale];
 }
